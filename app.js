e0756d232472ea955e6e92aca4d11014e855b60e
Santi2007939 <117610911+Santi2007939@users.noreply.github.com>
2023-12-26 10:25:51 -0500

    Add rectangles

--- app.js
+++ app.js
@@ -11,19 +11,27 @@ let p = 60;
 
 let svg = d3.select("svg");
 let tooltip = d3.select("#tooltip");
+let minYear;
+let maxYear;
 
 let genScales = () => {
+    minYear = d3.min(info, (i) => {return i["year"]});
+    maxYear = d3.max(info, (i) => {return i["year"]});
+
     xScale = d3.scaleLinear()
+                .domain([minYear, maxYear +1])
                 .range([p, w - p]);
 
     yScale = d3.scaleTime()
+                .domain([new Date(0, 0, 0, 0, 0, 0, 0), new Date(0, 12, 0, 0, 0, 0, 0)])
                 .range([p, h - p]);
 
 };
 
 
 let genAxes = () => {
-    let xAxis = d3.axisBottom(xScale);
+    let xAxis = d3.axisBottom(xScale)
+                    .tickFormat(d3.format("d"));
     svg.append("g")
             .call(xAxis)
             .attr("id", "x-axis")
@@ -43,7 +51,7 @@ let cells = () => {
             .append("rect")
             .attr("class", "cell")
             .attr("fill", (i) => {
-                variance = i["variance"]
+                variance = i["variance"];
                 if(variance <= -1) {
                     return "SteelBlue";
                 } else if (variance <= 0) {
@@ -51,13 +59,21 @@ let cells = () => {
                 } else if (variance <= 1) {
                     return "Orange";
                 } else {
-                    return "Crimsom";
+                    return "#DC143C";
                 }
             })
-
             .attr("data-year", (i) => {return i["year"]})
-            .attr("data-month", (i) => {return i["month"]})
-            .attr("data-temp", (i) => {return Number((base - i["variance"]).toFixed(1))});
+            .attr("data-month", (i) => {return i["month"] - 1})
+            .attr("data-temp", (i) => {return Number((base + i["variance"]).toFixed(1))})
+            .attr("height", (h - (2 * p))/12)
+            .attr("y", (i) => {return yScale(new Date(0, i["month"] -1, 0, 0, 0, 0, 0))})
+            .attr("width", (i) => {
+                let years = maxYear - minYear;
+                return (w - (2 * p))/years;
+            })
+            .attr("x", (i) => {
+                return xScale(i["year"]);
+            })
 };
 
 fetch(url)
